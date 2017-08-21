62e9c21413947aaf64dfe13df2691687ba9c40b6
Alex Meah <alexmeahdotcom@gmail.com>
2017-08-21 11:38:45 +0100

    Init

---
+++ index.js
@@ -0,0 +1,33 @@
+module.exports.rules = {
+    "no-moment": context => ({
+        MemberExpression: function (node) {
+            if (node.object.name === "moment") {
+                context.report(node, "Don't use \"moment\", use brightdates");
+            }
+        },
+        CallExpression: function (node) {
+            if (node.callee.name === "moment") {
+                context.report(node, "Don't use \"moment\", use brightdates");
+            }
+        }
+    }),
+    "no-date": context => ({
+        MemberExpression: function (node) {
+            if (node.object.name === "Date") {
+                context.report(node, "Don't use \"Date\", use brightdates");
+            }
+        },
+        CallExpression: function (node) {
+            if (node.callee.name === "Date") {
+                context.report(node, "Don't use \"Date\", use brightdates");
+            }
+        }
+    }),
+    "no-new-date": context => ({
+        NewExpression: function (node) {
+            if (node.callee.name === "Date") {
+                context.report(node, "Don't use \"new Date\", use brightdates");
+            }
+        }
+    })
+};
